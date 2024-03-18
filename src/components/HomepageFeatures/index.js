26015b735b559127618415e27c0078e02554da03
manish <Deepikamahajan31@gmail.com>
2024-03-18 11:13:46 +0530

    changes

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'React',
+    title: 'React.js',
     redirect: "/docs/category/react",
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default
   },
